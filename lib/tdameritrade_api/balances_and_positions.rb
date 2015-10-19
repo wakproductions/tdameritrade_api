fb79edf2a19ee0a80ff3e450cb41e3d8e3623a84
Winston Kotzan <wak@wakproductions.com>
2015-10-18 23:19:27 -0400

    Clean up balances and positions feature & build tests

--- lib/tdameritrade_api/balances_and_positions.rb
+++ lib/tdameritrade_api/balances_and_positions.rb
@@ -1,13 +1,17 @@
 module TDAmeritradeApi
   module BalancesAndPositions
-    BP_URL='https://apis.tdameritrade.com/apps/100/BalancesAndPositions'
-    
+    BALANCES_AND_POSITIONS_URL='https://apis.tdameritrade.com/apps/100/BalancesAndPositions'
+
     # +get_balances_and_positions+ get account balances
     # +options+ may contain any of the params outlined in the API docs
-    def get_balances_and_positions(options)
-      request_params = build_bp_params(options)
+    # * accountid - one of the account ids returned from the login service
+    # * type - type of data to be returned ('b' or 'p')
+    # * suppress_quotes - whether or not quotes should be suppressed on the positions (true/false)
+    # * alt_balance_format - whether or not the balances response should be returned in alternative format (true/false)
+    def get_balances_and_positions(account_id, options={})
+      request_params = build_bp_params(account_id, options)
 
-      uri = URI.parse BP_URL
+      uri = URI.parse BALANCES_AND_POSITIONS_URL
       uri.query = URI.encode_www_form(request_params)
 
       response = HTTParty.get(uri, headers: {'Cookie' => "JSESSIONID=#{@session_id}"}, timeout: 10)
@@ -28,8 +32,8 @@ module TDAmeritradeApi
 
     private
 
-    def build_bp_params(options)
-      req = {source: @source_id, accountid: @accounts[0][:account_id]}.merge(options)
+    def build_bp_params(account_id, options)
+      {source: @source_id, accountid: account_id}.merge(options)
     end
   end
 end
